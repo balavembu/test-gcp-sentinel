e52fb1a10cf0ec8d78366e41ff497e48a08f2be7
balavembu <90377726+balavembu@users.noreply.github.com>
2021-10-04 12:50:40 -0700

    Checking with Explicit Null

--- main.tf
+++ main.tf
@@ -17,9 +17,10 @@ resource "google_compute_instance" "vm_instance" {
 
   network_interface {
     # A default network is created for all GCP projects
-    # network = "${google_compute_network.vpc_network.self_link}"
-    # access_config {
-    # }
+    network = "${google_compute_network.vpc_network.self_link}"
+    access_config { 
+      null
+    }
   }
 }
 
