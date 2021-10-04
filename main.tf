46e12ff599bad6184ab451c6ec523443b381b230
balavembu <90377726+balavembu@users.noreply.github.com>
2021-10-04 12:48:25 -0700

    Moving Back

--- main.tf
+++ main.tf
@@ -15,12 +15,12 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 
-  # network_interface {
+  network_interface {
     # A default network is created for all GCP projects
     # network = "${google_compute_network.vpc_network.self_link}"
     # access_config {
     # }
-  # }
+  }
 }
 
 resource "google_compute_network" "vpc_network" {
