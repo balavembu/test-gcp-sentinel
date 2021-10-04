d383ce2ecf6f2c54d85cded48ad9f83a2643459c
balavembu <90377726+balavembu@users.noreply.github.com>
2021-10-04 00:42:38 -0700

    Commenting Properly

--- main.tf
+++ main.tf
@@ -17,8 +17,10 @@ resource "google_compute_instance" "vm_instance" {
 
   network_interface {
     # A default network is created for all GCP projects
-#     network = "${google_compute_network.vpc_network.self_link}"
-#   }
+    # network = "${google_compute_network.vpc_network.self_link}"
+    # access_config {
+    # }
+  }
 }
 
 resource "google_compute_network" "vpc_network" {
